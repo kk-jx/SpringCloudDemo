ef4c9144adbd19501faf4dd6275642c8dfe39285
huzl <hzl@maiqing-tech.com>
2017-11-28 10:56:35 +0800

    add spring cloud config module

---
+++ config-client-demo/src/main/java/cn/regent/springcloud/controller/TestController.java
@@ -0,0 +1,26 @@
+package cn.regent.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by kk on 2017/11/28.
+ */
+@RestController
+public class TestController {
+    @Value("${from}")
+    private String from;
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping(value="/from")
+    public Object from(){
+        String env_from = env.getProperty("from","undefined");
+        return this.from+"    "+env_from;
+    }
+
+}
--- config-server-demo/src/main/java/com/kk/springcloud/ConfigServerDemoApplication.java
+++ config-server-demo/src/main/java/com/kk/springcloud/ConfigServerDemoApplication.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-/**
- * Created by kk on 2017/11/27.
- */
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigServerDemoApplication {
 
-    public static void main(String[] args){
-        SpringApplication.run(ConfigServerApplication.class,args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerDemoApplication.class, args);
+	}
 }
